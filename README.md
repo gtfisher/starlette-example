0f61fbbcde324361c08459c7c3cab92e440c2750
gtfisher <grant@chaosiq.io>
2019-12-31 17:00:50 +0000

    initial commit of starlette with contact api and json store
    Signed-off-by: gtfisher <grant@chaosiq.io>

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# An example Starlette application
+# Starlette contact CRUD application
+
+This example is based on the example [Starlette] application from [Starlette example], but is
+adding a document database using and . This is based on a series of articles
+that show this for a node typescript solution [Web Api Node Typescript].
+
+[Starlette example]: [https://github.com/encode/starlette-example.git]
+[Starlette]: [https://www.starlette.io/]
+[Web Api Node Typescript]: [https://itnext.io/building-restful-web-apis-with-node-js-express-mongodb-and-typescript-part-1-2-195bdaf129cf?]
+
+## Install and run the Starlette Example
 
 Install and run:
 
@@ -22,3 +32,41 @@ Raise a server error by navigating to `http://127.0.0.1:8000/error`:
 ![Homepage](https://raw.githubusercontent.com/encode/starlette-example/master/docs/500.png)
 
 Switch the `app = Starlette(debug=True)` line to `app = Starlette()` to see a regular 500 page instead.
+
+## Add REST routes for CRM
+
+Now a contact rest service is going to be added that stores, retieves, updates and deletes json objects from an sqllite database using [dataset]
+
+### Create Contact
+
+Add code to handle the creation of a contact as a result of a POST request.
+
+```python
+@app.route('/api/contact', methods=['POST'])
+async def post_contact(request):
+    new_contact = await request.json()
+    new_contact['creationTime'] = int(time.time())
+    print(f"new contact: {new_contact}")
+    contact_table = db['contacts']
+    contact_table.insert(new_contact)
+    return JSONResponse({"created": "ok"})
+```
+
+The remainder of the rest methods can be seen in the source code []
+
+
+This can be tested with the following curl command
+```bash
+curl --header "Content-Type: application/json" \
+  --request POST \
+  --data '{"firstName":"Fred","lastName":"Smith","email":"fred.smith@test.com","company": "temp ltd", "phone": "012-345-67890"}' \
+  http://localhost:8000/api/contact
+```
+
+Once a contact record has been created with the command above the contacts list can be fetched with:
+
+```bash
+curl --header "Content-Type: application/json" http://localhost:8000/api/contact
+```
+
+[dataset]: [https://dataset.readthedocs.io/en/latest/index.html]
--- app.py
+++ app.py
@@ -1,13 +1,40 @@
+import datetime
+import dataset
+import json
+import sys
+import time
+
 from starlette.applications import Starlette
+from starlette.config import Config
 from starlette.staticfiles import StaticFiles
 from starlette.responses import HTMLResponse
+from starlette.responses import JSONResponse
+from starlette.responses import RedirectResponse
 from starlette.templating import Jinja2Templates
 import uvicorn
 
 
-templates = Jinja2Templates(directory='templates')
+# Configuration from environment variables or '.env' file.
+config = Config('.env')
+DATABASE_URL = config('DATABASE_URL')
 
-app = Starlette(debug=True)
+version = f"{sys.version_info.major}.{sys.version_info.minor}"
+templates = Jinja2Templates(directory='templates')
+db = dataset.connect(DATABASE_URL)
+
+
+def startup():
+    print('Ready to go')
+
+
+def get_items(table):
+    items = []
+    for item in db[table]:
+        items.append(item)
+    return items
+
+
+app = Starlette(debug=True, on_startup=[startup])
 app.mount('/static', StaticFiles(directory='statics'), name='static')
 
 
@@ -17,6 +44,66 @@ async def homepage(request):
     context = {"request": request}
     return templates.TemplateResponse(template, context)
 
+
+@app.route("/msg")
+async def message(request):
+    message = f"Hello world! From Starlette running on Uvicorn with Gunicorn in Alpine."
+    return JSONResponse({"message": message, "version": version})
+
+
+@app.route('/dt')
+async def my_date_time(request):
+    print(datetime.datetime.now())
+    return JSONResponse({'hello': 'world', 'now': str(datetime.datetime.now())})
+
+
+# contact rest api methods
+
+@app.route('/api/contact')
+async def get_contact(request):
+    return JSONResponse(get_items('contacts'))
+
+
+@app.route('/api/contact/{item_id}')
+async def get_contact_by_id(request):
+    cid = request.path_params['item_id']
+    contact = db['contacts'].find_one(id=cid)
+    return JSONResponse(contact)
+
+
+@app.route('/api/contact/{item_id}', methods=['PUT'])
+async def update_contact_by_id(request):
+    cid = request.path_params['item_id']
+    req_body = await request.json()
+    # data = dict(id=fid, type=req_body.type, count=req_body.count)
+    data = dict(id=cid,
+                firstName=req_body["firstName"],
+                lastName=req_body["lastName"],
+                email=req_body["email"],
+                company=req_body["company"],
+                phone=req_body["phone"],
+                creationTime=req_body["creationTime"])
+
+    count = db['contacts'].update(data, ['id'])
+    contact = db['contacts'].find_one(id=cid)
+    return JSONResponse(contact)
+
+
+@app.route('/api/contact/{item_id}', methods=['DELETE'])
+async def delete_contact_by_id(request):
+        fid = request.path_params['item_id']
+        cnt = db['contacts'].delete(id=fid)
+        return JSONResponse(get_items('contacts'))
+
+
+@app.route('/api/contact', methods=['POST'])
+async def post_contact(request):
+    new_contact = await request.json()
+    new_contact['creationTime'] = int(time.time())
+    print(f"new contact: {new_contact}")
+    contact_table = db['contacts']
+    contact_table.insert(new_contact)
+    return JSONResponse({"created": "ok"})
 
 @app.route('/error')
 async def error(request):
